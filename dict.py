af344b0cf4f7f55b208acf7410e636fd249a1a70
DIEGOHORVATTI <d.horvattid@gmail.com>
2022-08-14 19:56:25 -0400

    add: aula 16

--- dict.py
+++ dict.py
@@ -4,4 +4,49 @@ mangakas = {
   "Turtleme" : "The Beginning After The End"
 }
 
-print(mangakas["Masashi kishimoto"])
+mangakas["Kentaro Miura"] = "Berserk"
+
+print(mangakas["Masashi kishimoto"]) #print(mangakas.get("Masashi kishimoto"))
+
+mangakas["Masashi kishimoto"] = "Boruto"
+
+print("\n", mangakas, "\n")
+
+for x in mangakas:
+  print(x, ":", mangakas[x])   # chave, valor
+print("\n")
+
+for c,v in mangakas.items():
+  print(c, ":", v)   # chave, valor
+
+mangakas = {
+  "Eiichiro oda":{
+    "Naruto": 1999,
+    "Boruto": 2015
+  }
+}
+
+print("\n", mangakas["Eiichiro oda"], "\n")
+print(
+  "\n", 
+  list(mangakas["Eiichiro oda"].keys())[ 
+    (list(mangakas["Eiichiro oda"].values()).index(1999)) 
+  ],
+  ":", 
+  mangakas["Eiichiro oda"]["Naruto"], 
+  "\n"
+)
+
+letra_1 = { 1:"a" }
+
+letra_2 = { 2: "b" }
+
+letra_3 = { 3: "c" }
+
+letras = {
+  "L_1": letra_1,
+  "L_2": letra_2,
+  "L_3": letra_3
+}
+
+print(type(letras), ":" ,letras)
