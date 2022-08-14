cd2b3eacd2a68f34e32a95c68092b9d3d5cb2d76
DIEGOHORVATTI <d.horvattid@gmail.com>
2022-08-14 16:27:42 -0400

    add: aula 13

---
+++ if_else_elif.py
@@ -0,0 +1,24 @@
+teclado = input("\n Digite um número: ")
+
+if int(teclado) == 2:
+  print(" Iqual\n")
+elif int(teclado) > 2:
+  print(" Maior\n")
+else:
+  print(" Menor\n")
+
+"""
+Operador	Conceito	          Exemplo
+>         (Maior que)	        Verifica se um valor é maior que outro	x > 5
+<         (Menor que)	        Verifica se um valor é menor que outro	x < 5
+==        (Igual a)	          Verifica se um valor é igual a outro	x == 5
+!=        (Diferente de)      Verifica se um valor é diferente de outro	x != 5
+>=        (Maior ou igual a)	Verifica se um valor é maior ou igual a outro	x >= 5
+<=        (Menor ou igual a)	Verifica se um valor é menor ou igual a outro	x <= 5
+"""
+
+"""
+Operador	Conceito
+or        (ou)
+and       (e)
+"""
---
+++ loop.py
@@ -0,0 +1,13 @@
+import os
+os.system('clear')
+
+onePiece = ["Luffy", "Zoro", "Usoop", "Nami", "Robin"]
+
+for x in onePiece:
+  print(x)
+print('\n')
+
+x = 0
+while x < len(onePiece):
+  print(onePiece[x])
+  x += 1
