9fb040cb47586cc8e8bf4ea451399d1f8ecf640e
DIEGOHORVATTI <d.horvattid@gmail.com>
2022-08-14 20:41:35 -0400

    add: aula 21

--- def_function.py
+++ def_function.py
@@ -1,4 +1,22 @@
+# parametros referencia
 def soma(x, y):
   print(int(x+y))
 
 soma(2, 2)
+print("\n")
+
+# parametros abtrarios
+def soma(*x):
+  cache=0
+  for y in x: 
+    y += cache
+    cache=y
+  print(int(y))
+soma(2, 10, 10)
+print("\n")
+
+# parametros defult
+def livros(title="The seven legends"):
+  print(title)
+livros()
+livros("Linux a biblia")
